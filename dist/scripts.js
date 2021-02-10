1631ad5a61a36c17cf0c4030659bd05bacbb81ac
John Andrey Galingan <68799061+johnrookie-coder@users.noreply.github.com>
2021-02-10 08:15:17 -0800

    Major Changes with Hamburger Menu added

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,16 +1,20 @@
 // UI Variables
 const uiNavLinks = document.querySelectorAll('.nav-link');
-const links = ['#intro-CSS', '#border', '#color', '#combinators','#units', '#fonts','#margin', '#opacity', '#overflow', '#padding', '#position', '#pseudo-class', '#pseudo-element'];
+const uiLinks = ['#intro-CSS', '#border', '#color', '#combinators','#units', '#fonts','#margin', '#opacity', '#overflow', '#padding', '#position', '#pseudo-class', '#pseudo-element'];
+const uiMaindoc = document.querySelector('#main-doc');
+const uiHamburgerMenu = document.querySelector('.bars')
 
-uiNavLinks.forEach((link,idx)=> {
-  link.addEventListener('click', () => {
-    removeClass();
-      
-    link.classList.add('active'); 
-    let mLinks = links[idx]
-    newLink(mLinks);
-  })
-})
+// Links
+    uiNavLinks.forEach((link,idx)=> {
+      link.addEventListener('click', (e) => {
+        removeClass();
+          
+        link.classList.add('active'); 
+        let mLinks = uiLinks[idx]
+        newLink(mLinks);
+        e.stopPropagation();
+      })
+    })
 
 function removeClass() {
   uiNavLinks.forEach(link => {
@@ -20,17 +24,26 @@ function removeClass() {
 
 
 // Back to Top
- const uiBackToTop = document.querySelector('.back-To-Top');
+  const uiBackToTop = document.querySelector('.back-To-Top');
 
-window.addEventListener('scroll', () => {
-  if(window.pageYOffset > 2000){
-    uiBackToTop.classList.add('active')
-  }
-  else{
-    uiBackToTop.classList.remove('active')
-  }
-})
+  window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 2000){
+      uiBackToTop.classList.add('active')
+    }
+    else{
+      uiBackToTop.classList.remove('active')
+    }
+  })
 
-function newLink(link) {
-  window.location.assign(link);
-}
+  function newLink(link) {
+    window.location.assign(link);
+  }
+
+
+// HAMBURGER MENU
+  uiHamburgerMenu.addEventListener('click', showList);
+  function showList() {
+    uiHamburgerMenu.classList.toggle('active')
+    uiMaindoc.classList.toggle('active')
+  }
+
